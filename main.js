400d52bf2f317489081500ab8e4ee211dfd081a4
johnnydimitri <johnnydimitrisouza@hotmail.com>
2026-03-18 18:47:25 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,41 @@
+//document.addEventListener('DOMContentLoaded', function() {
+//    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+ //       const xhttp = new XMLHttpRequest();
+   //     const cep = document.getElementById('cep').value;
+    //    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+      //  xhttp.open('GET', endpoint);
+       // xhttp.send();
+  //  })
+//})
+
+$(document).ready(function() {
+    $('#cep').mask('00000-000');
+
+     $('#btn-buscar-cep').click(function() {
+         const cep = $('#cep').val();
+         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+         const botao = $(this);
+         $(botao).find('i').addClass('d-none');
+         $(botao).find('span').removeClass('d-none');
+
+        fetch(endpoint).then(function(reposta) {
+            return reposta.json()
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco)
+        })
+        .catch(function(erro) {
+            alert("ocorreu um erro ao buscar o endereço, favor tente novamente.")
+        })
+        .finally(function() {
+            $(botao).find('i').removeClass('d-none');
+            $(botao).find('span').addClass('d-none');
+        })
+     })
+})
